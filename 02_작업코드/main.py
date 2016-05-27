45181a425fdbbaab5cc321c46df86fc3c83c45d2
Ssenilen <madoungking@naver.com>
2016-05-28 03:53:07 +0900

    4가지 항목  > 1. 도서관  > 2. 안심/모범 음식점  > 3. 마트  > 4. 문화공간 검색 기능 추가

--- 02_작업코드/main.py
+++ 02_작업코드/main.py
@@ -4,23 +4,31 @@
 """
 
 import ListGoodFoodService
+import ListMarketInfoServer
+import GeoInfoLibrary
+import SearchCulturalFacilitiesDetailService
 
 loopFlag = -1
 
 def PrintMenu():
     print("[서울시 근린시설 검색 APP]")
-    print(" 1. 공공화장실 검색")
-    print(" 2. 도서관 검색")
-    print(" 3. 안심/모범 음식점 검색")
-    print(" 4. 마트 검색")
-    print(" 5. 공원 검색")
+    print(" 1. 도서관 검색")
+    print(" 2. 안심/모범 음식점 검색")
+    print(" 3. 마트 검색")
+    print(" 4. 문화공간 검색")
     print(" 9. 프로그램 종료")
 
 
 def SelectMenu(SelectKey):
-    if SelectKey == '3':
-        ListGoodFoodService.main()
     
+    if SelectKey == '1':
+        GeoInfoLibrary.main()
+    elif SelectKey == '2':
+        ListGoodFoodService.main()
+    elif SelectKey == '3':
+        ListMarketInfoServer.main()
+    elif SelectKey == '4':
+        SearchCulturalFacilitiesDetailService.main()
     elif SelectKey == '9':
         global loopFlag
         loopFlag = 0
